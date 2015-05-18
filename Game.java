121f2f3b08bde51b92f0cb88721c7f83ef485b4d
xpgrogan <xavier.grogan@aol.com>
2015-05-17 20:36:02 -0500

    Game.java
    reverted

--- Game.java
+++ Game.java
@@ -1,8 +1,6 @@
 import java.util.*;
-import javax.swing.*;
-import java.awt.*;
 
-public class Game extends JFrame
+public class Game
 {
 	public static boolean hit(int ap, int fl)
 	{
@@ -17,35 +15,25 @@ public class Game extends JFrame
 	
 	public static void main (String [] args)
 	{
-		//Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		Enemy e = new Enemy();
 		int flash;
 		int score = 0;
 		boolean hitOrMiss = false;
-		//JOptionPane.showInputDialog("Enter your name");
 		
 		while(hitOrMiss == false)
 		{
 			e = new Enemy();
 			if(score < 5)
-			{
-				flash = Integer.parseInt(JOptionPane.showInputDialog("SUMMONING ENEMY!\n"+
-													e.name +"\n" + e.description +
-													"\nUsing attack: " +e.a.name+
-													"\nYour score so far: " + score+
-													"\nWhich was will you flash?!?!?!?!\n"+
-													"Enter 1 for left, 2 for middle(stay), or 3 for right!\n"+
-													"Choice: "));
-				
-				
-//				System.out.println("SUMMONING ENEMY!\n");
-//				System.out.println(e.name);
-//				System.out.println(e.description);
-//				System.out.println("\nUsing attack: " + e.a.name);
-//				System.out.println("\nYour score so far: " + score);
-//				System.out.println("\nWhich way will you flash?!?!?!?!\n");
-//				System.out.println("Enter 1 for left, 2 for middle(stay), or 3 for right!\n Choice: ");
-//				flash = scan.nextInt();
+			{	
+				System.out.println("SUMMONING ENEMY!\n");
+				System.out.println(e.name);
+				System.out.println(e.description);
+				System.out.println("\nUsing attack: " + e.a.name);
+				System.out.println("\nYour score so far: " + score);
+				System.out.println("\nWhich way will you flash?!?!?!?!\n");
+				System.out.println("Enter 1 for left, 2 for middle(stay), or 3 for right!\n Choice: ");
+				flash = scan.nextInt();
 		
 				if(flash == 1)
 					flash = 0;
@@ -55,30 +43,26 @@ public class Game extends JFrame
 					flash = 2;
 				else
 				{
-					JOptionPane.showMessageDialog(null, "You broke it!!!!!! :(");
-					//System.out.println("Broke it");
+					System.out.println("You broke it");
 					System.exit(1);
 				}
 		
 				hitOrMiss = hit(e.a.position, flash);
 				if(hitOrMiss == false)
 				{
-					JOptionPane.showMessageDialog(null,"You live to fight another day on the Fields of Justice!");
-					//System.out.println("You live to fight another day on the Fields of Justice!");
+					System.out.println("You live to fight another day on the Fields of Justice!");
 					score += 1;
 				}	
 				else
 				{
-					JOptionPane.showMessageDialog(null,"You got #REKT m8!!!");
-					JOptionPane.showMessageDialog(null, "DEFEAT!!!");
-					//System.out.println("You got #REKT m8!!!");
+					System.out.println("You got #REKT m8!!!");
+					System.out.println("DEFEAT");
 					System.exit(1);
 				}
 			}
 			else
 			{
-				JOptionPane.showMessageDialog(null, "VICTORY!!!!!");
-				//System.out.println("VICTORY!!!!!!!");
+				System.out.println("VICTORY!!!!!!!");
 				System.exit(1);
 			}
 		}	
